38ae62637192ce7b7333adf1d7c8840e082623b0
mladen314 <144899141+mladen314@users.noreply.github.com>
2026-01-30 13:36:04 +0100

    260130

--- IIIM25/src/cas_260123/Main.java
+++ IIIM25/src/cas_260123/Main.java
@@ -5,23 +5,19 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Ucenici x = new Ucenici();
-		x.ime="Marko";
-		x.prezime = "Markovic";
-		x.razred = 3;
-		x.srednja_ocjena = 3.51;
+		Ucenici x = new Ucenici("Marko", "Markovic", 3, 3.51);
 		
-		Ucenici y = new Ucenici();
-		y.ime="Ana";
-		y.prezime = "Andric";
-		y.razred = 4;
-		y.srednja_ocjena = 4.51;
+	
+		Ucenici y = new Ucenici("Ana", "Andric", 4, 4.51, 4321);
 		
 		Ucenici z = y;
 		
-		System.out.println("Ucenik "+y.ime+" je "+y.razred+". razred.");
+		//System.out.println("Ucenik "+y.ime+" je "+y.razred+". razred.");
 		
-		
+		x.info();
+		y.info();
+		System.out.println(x.ispravan_pin(123));
+		//Ucenici.f();
 	}
 
 }
--- IIIM25/src/cas_260123/Ucenici.java
+++ IIIM25/src/cas_260123/Ucenici.java
@@ -6,5 +6,39 @@ public class Ucenici {
 	String prezime;
 	int razred;
 	double srednja_ocjena;
+	private int pin = 1234;
+	
+	
+	
+	public Ucenici(String ime, String prezime, int razred, double srednja_ocjena, int pin) {
+		this.ime = ime;
+		this.prezime = prezime;
+		this.razred = razred;
+		this.srednja_ocjena = srednja_ocjena;
+		this.pin = pin;
+		
+		this.info();
+		
+	}
+	public Ucenici(String ime, String prezime, int razred, double srednja_ocjena) {
+		this.ime = ime;
+		this.prezime = prezime;
+		this.razred = razred;
+		this.srednja_ocjena = srednja_ocjena;
+		
+		this.info();
+	}
+
+	void info() {
+		System.out.println("Ovaj objekat je "+ime+" "+prezime);
+	}
+	
+	boolean ispravan_pin(int x) {
+		return x==pin;
+	}
+	/*
+	static void f() {
+		System.out.println("RADI");
+	}*/
 
 }
